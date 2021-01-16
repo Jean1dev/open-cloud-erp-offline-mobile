7366a792cfc570162741e36dd09d6e499158cafd
Jean <jeanluca.fernandes@totvs.com.br>
2021-01-16 18:15:12 -0300

    v1 release

--- screens/Vendas.tsx
+++ screens/Vendas.tsx
@@ -1,46 +1,105 @@
 //@ts-nocheck
 import * as React from 'react';
-import { readProduto, readCliente } from '../storage';
-import { ScrollView, StyleSheet, Image, View } from 'react-native';
-import { List, Text, Chip, Divider, useTheme } from 'react-native-paper';
+import { readProduto, readCliente, storeVendas } from '../storage';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import {
+  List,
+  IconButton,
+  Avatar,
+  Divider,
+  Card,
+  Button,
+  TextInput
+} from 'react-native-paper';
 
 export default function Vendas() {
   const [step, setStep] = React.useState(1)
   const [produtos, setProdutos] = React.useState([])
   const [clientes, setClientes] = React.useState([])
   const [venda, setVenda] = React.useState({})
+  const [alreadyLoad, setAlreadyLoad] = React.useState(false)
 
   const setValues = (propName, propValue) => {
     setVenda({
       ...venda,
       [propName]: propValue
     })
-
-    console.log({
-      ...venda,
-      [propName]: propValue
-    })
   }
 
-  const escolherCliente = React.useCallback((id) => {
-    setValues('cliente', id)
+  const escolherCliente = React.useCallback((cliente) => {
+    setValues('cliente', cliente)
     setStep(3)
   }, [venda])
 
   const addProduto = React.useCallback((prod) => {
     const list = venda.itens ? venda.itens : []
+    if (list.length) {
+      const produtoJaAdicionado = list.filter(produtos => produtos.id === prod.id)
+
+      if (produtoJaAdicionado[0]) {
+        const noCarrinho = list.filter(produtos => produtos.id != prod.id)
+        prod.quantidade = produtoJaAdicionado[0].quantidade + 1
+        noCarrinho.push(prod)
+        setValues('itens', noCarrinho)
+        return
+      }
+    }
     prod.quantidade = 1
     list.push(prod)
     setValues('itens', list)
   }, [venda])
 
-  React.useEffect(() => {
-    readProduto().then(values => setProdutos(values))
-  }, [])
+  const plusProduto = React.useCallback((prod) => {
+    prod.quantidade++
+    const carrinho = venda.itens.filter(produto => produto.id != prod.id)
+    carrinho.push(prod)
+    setValues('itens', carrinho)
+  }, [venda])
+
+  const minusProduto = React.useCallback((prod) => {
+    prod.quantidade--
+    const carrinho = venda.itens.filter(produto => produto.id != prod.id)
+    if (prod.quantidade <= 0) {
+      setValues('itens', carrinho)
+      return
+    }
+    carrinho.push(prod)
+    setValues('itens', carrinho)
+  }, [venda])
+
+  const doVenda = React.useCallback(() => {
+    const vendaCopy = venda
+    vendaCopy.uuid = Math.random()
+    vendaCopy.valorRecebido = venda.valorRecebido || totalVenda
+    console.log(vendaCopy)
+    storeVendas(vendaCopy).then(() => {
+      setStep(1)
+      setVenda({})
+    })
+  }, [venda])
+
+  const totalVenda = React.useMemo(() => {
+    if (venda.itens) {
+      return venda.itens.reduce((sum, item) => sum + (item.quantidade * item.valorVenda), 0)
+    }
+
+    return 0
+  }, [venda])
 
   React.useEffect(() => {
-    readCliente().then(values => setClientes(values))
-  }, [])
+    readProduto().then(values => {
+      setProdutos(values || [])
+      setAlreadyLoad(true)
+    })
+  }, [alreadyLoad])
+
+  React.useEffect(() => {
+    readCliente().then(values => setClientes(values || []))
+  }, [alreadyLoad])
+
+  if (alreadyLoad && produtos.length === 0) {
+    setAlreadyLoad(false)
+  }
 
   if (step === 1) {
     return (
@@ -55,7 +114,7 @@ export default function Vendas() {
               key={prod.id}
               onPress={() => addProduto(prod)}
               left={props => <List.Icon {...props} icon="folder" />}
-              right={props => <List.Icon {...props} icon="cart" />}
+              right={props => <List.Icon {...props} icon="plus" />}
             />
           ))}
         </List.Section>
@@ -68,7 +127,7 @@ export default function Vendas() {
     return (
       <ScrollView style={styles.container}>
         <List.Section>
-        <List.Subheader onPress={() => setStep(3)}>Fechar a Venda</List.Subheader>
+          <List.Subheader onPress={() => setStep(3)}>Fechar a Venda</List.Subheader>
           <Divider />
           <List.Subheader onPress={() => setStep(1)}>Selecionar Produtos</List.Subheader>
           <Divider />
@@ -77,7 +136,7 @@ export default function Vendas() {
             <List.Item
               title={cliente.nome}
               key={cliente.id}
-              onPress={() => escolherCliente(cliente.id)}
+              onPress={() => escolherCliente(cliente)}
               left={props => <List.Icon {...props} icon="account-plus-outline" />}
             />
           ))}
@@ -88,7 +147,52 @@ export default function Vendas() {
   }
 
   return (
-    <View></View>
+    <ScrollView style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title={`Total da venda R$: ${totalVenda}`} />
+        <Card.Content>
+          <Divider />
+          <TextInput
+            label="Valor recebido"
+            keyboardType="numeric"
+            value={venda.valorRecebido}
+            onChangeText={val => setValues('valorRecebido', val)}
+          />
+          <Divider />
+          <Button icon="cart" mode="contained" onPress={doVenda}>
+            Vender
+        </Button>
+        </Card.Content>
+      </Card>
+
+      <Card style={styles.card}>
+        <Card.Title
+          title={venda?.cliente?.nome}
+          subtitle="Cliente Selecionado"
+          left={(props) => <Avatar.Icon {...props} icon="account" />}
+        />
+
+      </Card>
+
+
+      {venda?.itens?.map(produto => (
+        <Card key={produto.id}>
+          <Card.Title
+            title={produto.nome}
+            subtitle={`Quantidade: ${produto.quantidade} - R$: ${produto.valorVenda}`}
+
+            right={(props) => (
+              <View>
+                <IconButton {...props} icon="plus-circle-outline" onPress={() => plusProduto(produto)} />
+                <IconButton {...props} icon="tray-remove" onPress={() => minusProduto(produto)} />
+              </View>
+            )}
+          />
+          <Divider />
+        </Card>
+      ))}
+
+    </ScrollView>
   )
 
 }
@@ -107,5 +211,8 @@ const styles = StyleSheet.create({
   },
   column: {
     flexDirection: 'column',
+  },
+  card: {
+    margin: 4,
   },
 });
---
+++ service/Sincronizacao.ts
@@ -0,0 +1,70 @@
+//@ts-nocheck
+import api from './api';
+import {
+    storeCliente,
+    storeProduto,
+    readVendas,
+    overwriteVendas
+} from '../storage';
+
+function removeVenda(venda) {
+    readVendas().then(vendas => {
+        const restantes = vendas.filter(element => element.uuid != venda.uuid)
+        overwriteVendas(restantes).then(() => console.log('venda removida ', venda.uuid))
+    })
+}
+
+export default async (callback: Function) => {
+    callback('Sincronizando clientes...')
+
+    api.get('cliente').then(resultado => {
+
+        storeCliente(resultado.data).then(() => {
+            callback('Sincronizando produtos...')
+
+            api.get('produto').then(resultado => {
+
+                storeProduto(resultado.data).then(() => {
+                    callback('Carregando vendas ...')
+
+                    readVendas().then(vendas => {
+                        if (!vendas || vendas == undefined) return
+
+                        let total = vendas.length
+                        if (total == 0) {
+                            callback('não há nada pendente')
+                            return
+                        }
+
+                        vendas.map(venda => ({
+                            mobile: true,
+                            cliente: venda.cliente.id,
+                            valorRecebido: venda.valorRecebido,
+                            itens: venda.itens.map(produto => ({
+                                valorUnitario: produto.valorVenda,
+                                quantidade: produto.quantidade,
+                                produtoId: produto.id
+                            })),
+                            uuid: venda.uuid
+                          })).forEach(venda => {
+                            api.post('venda', venda).then(async () => {
+                                total--
+                                console.log('venda sincronizada')
+                                await removeVenda(venda)
+
+                                if (total == 0) {
+                                    callback('concluido')
+                                }
+                            }).catch(() => {
+                                console.log('ocorreu um erro ao sincronizar a venda')
+                                callback('algumas vendas não foram sincronizadas')
+                            })
+
+
+                          })
+                    })
+                })
+            })
+        })
+    })
+}
